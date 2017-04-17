3980133132747179dff04eb796c798fc5f77ad0d
turanct <spinnewebber_toon@hotmail.com>
2017-04-17 17:30:47 +0200

    Add a getById method to the products repository

--- src/Runs/Products.php
+++ src/Runs/Products.php
@@ -5,11 +5,22 @@ namespace CoffeeRun\Runs;
 interface Products
 {
     /**
+     * Get a Product by its id
+     *
+     * @param ProductId $id The product's id
+     *
+     * @throws InvalidArgumentId when the product is not found
+     *
+     * @return Product A product
+     */
+    public function getById(ProductId $id);
+
+    /**
      * Get a list of Products available in a shop
      *
-     * @param ShopId The shop's id
+     * @param ShopId $shopId The shop's id
      *
      * @return Product[] A list of products
      */
-    public function forShop(ShopId $id);
+    public function forShop(ShopId $shopId);
 }
